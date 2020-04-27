70f9995adb5b91084705e53e5c964d9f620757b3
Pedro <pether.1994@gmail.com>
2020-04-26 23:13:44 -0500

    auth

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -10,10 +10,10 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 class Handler extends ExceptionHandler
 {
@@ -61,47 +61,49 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
-        if($exception instanceof ValidationException){
-            return $this->convertValidationExceptionToResponse($exception,$request);
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
         }
 
-        if($exception instanceof ModelNotFoundException){
-            $model =strtolower((class_basename($exception->getModel())));
-            return $this->errorResponse("No existe ninguna instancia de {$model} con el id especificado.",404);
+        if ($exception instanceof ModelNotFoundException) {
+            $model = strtolower((class_basename($exception->getModel())));
+            return $this->errorResponse("No existe ninguna instancia de {$model} con el id especificado.", 404);
         }
 
-        if($exception instanceof AuthenticationException ){
-            return $this->unauthenticated($request,$exception);
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
         }
 
-        if($exception instanceof AuthorizationException){
+        if ($exception instanceof AuthorizationException) {
             return $this->errorResponse("No posee los privilegios necesarios para aceder a este sitio.", 403);
         }
 
-        if($exception instanceof NotFoundHttpException){
+        if ($exception instanceof NotFoundHttpException) {
             return $this->errorResponse("No se encontró la URL especificada.", 404);
         }
 
-        if($exception instanceof MethodNotAllowedHttpException){
+        if ($exception instanceof MethodNotAllowedHttpException) {
             return $this->errorResponse("El metodo especificado en la peticion no es valido.", 405);
-
         }
 
-        if($exception instanceof HttpException){
-            return $this->errorResponse($exception->getMessage(),$exception->getStatusCode());
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
         }
-        if($exception instanceof QueryException){
+        if ($exception instanceof QueryException) {
             $code = $exception->errorInfo[1];
-            if($code == 1451){
-                return $this->errorResponse('No se puede eliminar el recurso porque está relacionado con otro.',409);
+            if ($code == 1451) {
+                return $this->errorResponse('No se puede eliminar el recurso porque está relacionado con otro.', 409);
             }
         }
 
-        if(config('app.debug')){
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()->back()->withInput($request->input());
+        }
+
+        if (config('app.debug')) {
             return parent::render($request, $exception);
         }
-        return $this->errorResponse('Falla inesperada.Intentelo más tarde',500);
-
+        return $this->errorResponse('Falla inesperada.Intentelo más tarde', 500);
     }
 
     /**
@@ -113,13 +115,29 @@ class Handler extends ExceptionHandler
      */
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
-       $errors = $e-> validator->errors()->getMessages();
 
-        return $this->errorResponse($errors,422);
+        $errors = $e->validator->errors()->getMessages();
+
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($errors, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
+
+        return $this->errorResponse($errors, 422);
     }
 
     protected function unauthenticated($request, AuthenticationException $exception)
     {
-        return $this->errorResponse('No autenticado',401);
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
+        return $this->errorResponse('No autenticado', 401);
+    }
+
+    private function isFrontend($request)
+    {
+        return $request->acceptsHtml() && collect($request->route()->middleware())->contains('web');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Auth\AuthManager;
+use Illuminate\Auth\Middleware\Authenticate;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +15,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//LOGIN
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
 
-Auth::routes();
+//PASSWORD
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('guest');
